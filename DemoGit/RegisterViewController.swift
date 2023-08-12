c803e761a72fd4eb99a12fec961f1f69afcd8e65
nganphan0695 <nganvp0695@gmail.com>
2023-08-12 18:07:01 +0700

    commit

--- DemoGit/RegisterViewController.swift
+++ DemoGit/RegisterViewController.swift
@@ -11,12 +11,28 @@ class RegisterViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        register()
+        result()
         login()
     }
     
-    func register() {
-        // Fix xong bug
+    func result() {
+        var array: [Int] = [2, 3, 7, 8, 6, 10, 15, 16, 19]
+        
+            for i in array {
+                if i % 2 != 0 {
+                    print("số lẻ đầu tiên là \(i)")
+                    break
+                }
+            }
+        var arrayDao = (array.reversed())
+        
+            for k in arrayDao {
+                if k % 2 == 0 {
+                    print("số chẵn cuối cùng là \(k)")
+                    break
+                }
+            }
+        
     }
     
     func login() {
